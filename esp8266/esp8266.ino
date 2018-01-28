ee31be6a9abdef846921d1c81f6fd77697be1bb7
Cédric JUNG <cedced19@gmail.com>
2018-01-28 16:23:12 +0100

    Do request each 5 minutes

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,5 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
 
 const int detect =  2;
 
@@ -13,19 +18,23 @@ void setup () {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
   }
- 
+  WiFi.mode(WIFI_STA);
+
 }
 
 void loop () {
   if (WiFi.status() == WL_CONNECTED) {
     if (digitalRead(detect)) { 
       HTTPClient http;  
-   
-      http.begin("http://192.168.0.47:8000/detected"); // URL to request
+      http.begin("http://192.168.0.40:7770/water-detected"); // URL to request
       http.GET();                                                                 
-   
+      http.end(); 
+    } else {
+      HTTPClient http;  
+      http.begin("http://192.168.0.40:7770/water-detector-connected"); // URL to request
+      http.GET();                                                                 
       http.end(); 
     }
+    delay(300000);
   }
-  delay(10000); // delay between two detections 
 }
